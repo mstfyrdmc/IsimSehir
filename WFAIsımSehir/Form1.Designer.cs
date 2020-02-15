95f8f60f8a6afd55faa1a827559afe58ca315de8
mstfyrdmc <45285832+AdamGiby@users.noreply.github.com>
2020-02-15 19:12:21 +0300

    IsimSehir

---
+++ WFAIsımSehir/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WFAIsımSehir
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.lblAd = new System.Windows.Forms.Label();
+            this.lblSehir = new System.Windows.Forms.Label();
+            this.btnAd = new System.Windows.Forms.Button();
+            this.btnSehir = new System.Windows.Forms.Button();
+            this.btnEsle = new System.Windows.Forms.Button();
+            this.txtSehir = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtAd
+            // 
+            this.txtAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAd.Location = new System.Drawing.Point(49, 53);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(190, 35);
+            this.txtAd.TabIndex = 0;
+            // 
+            // lblAd
+            // 
+            this.lblAd.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAd.Location = new System.Drawing.Point(49, 145);
+            this.lblAd.Name = "lblAd";
+            this.lblAd.Size = new System.Drawing.Size(190, 206);
+            this.lblAd.TabIndex = 1;
+            // 
+            // lblSehir
+            // 
+            this.lblSehir.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSehir.Location = new System.Drawing.Point(298, 145);
+            this.lblSehir.Name = "lblSehir";
+            this.lblSehir.Size = new System.Drawing.Size(205, 206);
+            this.lblSehir.TabIndex = 1;
+            // 
+            // btnAd
+            // 
+            this.btnAd.Location = new System.Drawing.Point(49, 106);
+            this.btnAd.Name = "btnAd";
+            this.btnAd.Size = new System.Drawing.Size(75, 23);
+            this.btnAd.TabIndex = 2;
+            this.btnAd.Text = "İsim Ekle";
+            this.btnAd.UseVisualStyleBackColor = true;
+            this.btnAd.Click += new System.EventHandler(this.btnAd_Click);
+            // 
+            // btnSehir
+            // 
+            this.btnSehir.Location = new System.Drawing.Point(298, 106);
+            this.btnSehir.Name = "btnSehir";
+            this.btnSehir.Size = new System.Drawing.Size(75, 23);
+            this.btnSehir.TabIndex = 2;
+            this.btnSehir.Text = "Şehir Ekle";
+            this.btnSehir.UseVisualStyleBackColor = true;
+            this.btnSehir.Click += new System.EventHandler(this.btnSehir_Click);
+            // 
+            // btnEsle
+            // 
+            this.btnEsle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEsle.Location = new System.Drawing.Point(382, 373);
+            this.btnEsle.Name = "btnEsle";
+            this.btnEsle.Size = new System.Drawing.Size(118, 44);
+            this.btnEsle.TabIndex = 2;
+            this.btnEsle.Text = "Eşleştir";
+            this.btnEsle.UseVisualStyleBackColor = true;
+            this.btnEsle.Click += new System.EventHandler(this.btnEsle_Click);
+            // 
+            // txtSehir
+            // 
+            this.txtSehir.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSehir.Location = new System.Drawing.Point(298, 53);
+            this.txtSehir.Name = "txtSehir";
+            this.txtSehir.Size = new System.Drawing.Size(202, 35);
+            this.txtSehir.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 461);
+            this.Controls.Add(this.btnEsle);
+            this.Controls.Add(this.btnSehir);
+            this.Controls.Add(this.btnAd);
+            this.Controls.Add(this.lblSehir);
+            this.Controls.Add(this.lblAd);
+            this.Controls.Add(this.txtSehir);
+            this.Controls.Add(this.txtAd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label lblAd;
+        private System.Windows.Forms.Label lblSehir;
+        private System.Windows.Forms.Button btnAd;
+        private System.Windows.Forms.Button btnSehir;
+        private System.Windows.Forms.Button btnEsle;
+        private System.Windows.Forms.TextBox txtSehir;
+    }
+}
+
---
+++ WFAIsımSehir/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAIsımSehir
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+
+        string[] isimler = new string[15];
+        string[] sehirler = new string[15];
+        int ad = 0;
+        int sehir = 0;
+
+        private void btnAd_Click(object sender, EventArgs e)
+        {
+            isimler[ad] = txtAd.Text;
+            lblAd.Text += txtAd.Text + "\n";
+            ad++;
+            txtAd.Text = string.Empty;
+        }
+
+        private void btnSehir_Click(object sender, EventArgs e)
+        {
+            sehirler[sehir] = txtSehir.Text;
+            lblSehir.Text += txtSehir.Text + "\n";
+            sehir++;
+            txtSehir.Text = string.Empty;
+        }
+
+        private void btnEsle_Click(object sender, EventArgs e)
+        {
+            int a = rnd.Next(0, ad-1);
+            int s = rnd.Next(0, sehir-1);
+
+            MessageBox.Show("Eşleştirme: " + isimler[a] + " " + sehirler[s]);
+
+            isimler[a].Remove(a, a);
+            sehirler[s].Remove(s, s);
+
+          //  lblAd.Text = isimler[];
+          //  lblSehir.Text = sehirler.ToString();
+
+            
+
+        }
+    }
+}
